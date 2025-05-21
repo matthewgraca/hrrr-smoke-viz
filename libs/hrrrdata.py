f65ad85a8b3a7ebe90bf514d1eb0fef91123aeda
Matthew Graca <matthew.graca@gmail.com>
2025-05-21 14:43:38 -0700

    moved comments, changed date range, half implemented 104 error handling
    1. Moved comments to appropriate location
    2. Implemented handling for 104 error; found FastHerbie issue where it logs instead of throwing exception. Will need to implement FastHerbie ourselves.
    3. Made note of extent bug

--- libs/hrrrdata.py
+++ libs/hrrrdata.py
@@ -7,27 +7,11 @@ import xarray as xr
 import cv2
 
 class HRRRData:
-    '''
-    Gets the HRRR data.
-    Pipeline:
-        - Use Herbie to download the data as grib files
-        - Use wgrib2 to subregion the grib files
-        - Convert the grib files to xarray, then into numpy as frames
-            - (num_frames, row, col)
-        - Interpolate and add a channel axis to the array of frames
-            - (num_frames, row, col, channel)
-        - Create samples from a sliding window of frames
-            - (samples, frames, row, col, channel)
-
-    Members:
-        data: The complete processed HRRR data
-        herbie: The inventory of Herbie objects of the HRRR data
-    '''
     def __init__(
         self,
         start_date,
         end_date,
-        extent=None,
+        extent=None, #NOTE this fails, will attempt to subregion nothing
         extent_name='subset_region',
         product='MASSDEN',
         frames_per_sample=1,
@@ -35,6 +19,22 @@ class HRRRData:
         sample_setting=1,
         verbose=False
     ):
+        '''
+        Gets the HRRR data.
+        Pipeline:
+            - Use Herbie to download the data as grib files
+            - Use wgrib2 to subregion the grib files
+            - Convert the grib files to xarray, then into numpy as frames
+                - (num_frames, row, col)
+            - Interpolate and add a channel axis to the array of frames
+                - (num_frames, row, col, channel)
+            - Create samples from a sliding window of frames
+                - (samples, frames, row, col, channel)
+
+        Members:
+            data: The complete processed HRRR data
+            herbie: The inventory of Herbie objects of the HRRR data
+        '''
         # pipeline
         # frame-by-frame sampling
         if sample_setting == 1:
@@ -89,18 +89,48 @@ class HRRRData:
             )
             raise ValueError(" ".join(msg))
 
-    '''
-    Uses FastHerbie to grab the remote data, and download it; frame by frame.
+    def __attempt_download(self, date_range, product, forecast_range=[0]):
+        '''
+        Attempt to use FastHerbie to download HRRR data.
+        - Will handle reset connection (104), goes for 5 max attempts
+        - Any unhandled exception will just exit.
+        Returns the FastHerbie object, which is pretty much a list of Herbie objects.
+        '''
+        downloaded = False 
+        max_tries = 5
+        tries = 1 
+        backoff_seconds = 2 
+        while not downloaded and tries < max_tries:
+            try:
+                tries += 1
+                FH = FastHerbie(date_range, model="hrrr", fxx=forecast_range)
+                FH.download(product)
+                downloaded = True
+            except ConnectionResetError as e:
+                if tries == max_tries:
+                    print(
+                        f"❌ {tries}/{max_tries} attempts made.\n"
+                        f"Exiting..."
+                    )
+                    sys.exit(1)
+                print(
+                    f"⚠️  Error while downloading; {e}\n"
+                    f"Attempt number {tries}/{max_tries}, "
+                    f"backing off by {backoff_seconds} seconds."
+                )
+                time.sleep(backoff_seconds)
+                backoff_seconds *= 2
+            except Exception as e:
+                print(
+                    f"❌ Something went wrong.\n"
+                    f"Exception type: {type(e)}, and name:{type(e).__name__}\n"
+                    f"Exception: {e}\n"
+                    f"Exiting..."
+                )
+                sys.exit(1)
 
-    Arguments:
-        start_date: The start date of the query, in the form "yyyy-mm-dd-hh"
-        end_date: The end date of the query (inclusive)
-        product: regex of the product to download 
-        verbose: Determines if Herbie objects should be printed
+        return FH
 
-    Returns:
-        The list of Herbie objects of the downloaded data
-    '''
     def __get_hrrr_data_frame_by_frame(
         self, 
         start_date, 
@@ -108,30 +138,32 @@ class HRRRData:
         product, 
         verbose
     ):
+        '''
+        Uses FastHerbie to grab the remote data, and download it; frame by frame.
+
+        Arguments:
+            start_date: The start date of the query, in the form "yyyy-mm-dd-hh"
+            end_date: The end date of the query (exclusive)
+            product: regex of the product to download 
+            verbose: Determines if Herbie objects should be printed
+
+        Returns:
+            The list of Herbie objects of the downloaded data
+        '''
+        end_date = pd.to_datetime(end_date) - pd.Timedelta(hours=1)
         dates = pd.date_range(start_date, end_date, freq="1h")
-        FH = FastHerbie(dates, model="hrrr", fxx=[0])
-        FH.download(product)
+
+        FH = self.__attempt_download(
+            date_range=dates, 
+            product=product, 
+            forecast_range=[0]
+        )
 
         if verbose:
             [print(repr(H)) for H in FH.objects]
 
         return FH.objects
 
-    '''
-    Uses FastHerbie to grab the remote data, and download it; offset by the
-    number of frames per sample, and using forecasts.
-
-    Arguments:
-        start_date: The start date of the query, in the form "yyyy-mm-dd-hh"
-        end_date: The end date of the query (inclusive)
-        offset: The number of frames per sample we offset by
-        product: regex of the product to download 
-        verbose: Determines if Herbie objects should be printed
-
-    Returns:
-        The list of Herbie objects of the downloaded data
-
-    '''
     def __get_hrrr_data_offset_by_forecast(
         self, 
         start_date,
@@ -140,34 +172,55 @@ class HRRRData:
         product,
         verbose
     ):
+        '''
+        Uses FastHerbie to grab the remote data, and download it; offset by the
+        number of frames per sample, and using forecasts.
+
+        Arguments:
+            start_date: The start date of the query, in the form "yyyy-mm-dd-hh"
+            end_date: The end date of the query (exclusive)
+            offset: The number of frames per sample we offset by
+            product: regex of the product to download 
+            verbose: Determines if Herbie objects should be printed
+
+        Returns:
+            The list of Herbie objects of the downloaded data
+
+        '''
         # if sample is t=0, pull init @ 00, fxx = 01 so we provide next-sample forecast
         offset_start_date = pd.to_datetime(start_date) + pd.Timedelta(hours=offset - 1)
+        end_date = pd.to_datetime(end_date) - pd.Timedelta(hours=1)
         dates = pd.date_range(offset_start_date, end_date, freq="1h")
-        FH = FastHerbie(dates, model="hrrr", fxx=[i for i in range(1, offset + 1)])
-        FH.download(product)
+
+        FH = self.__attempt_download(
+            date_range=dates,
+            product=product,
+            forecast_range=[i for i in range(1, offset + 1)]
+        )
 
         if verbose:
             [print(repr(H)) for H in FH.objects]
 
         return FH.objects 
 
-    '''
-    Takes a list of Herbie objects, and subregions the downloaded grib files
-
-    Arguments:
-        herbie_data: The list containing the Herbie objects
-        extent: The bounding box with the shape: (a, b, c, d):
-            - a: bottom longitude
-            - b: top longitude
-            - c: bottom latitude
-            - d: top latitude
-        extent_name: Desired name of the subregion
-        product: Regex of the product that was downloaded
-
-    Returns:
-        A list of the subregioned grib files
-    '''
     def __subregion_grib_files(self, herbie_data, extent, extent_name, product):
+        '''
+        Takes a list of Herbie objects, and subregions the downloaded grib 
+        files. If there is no defined extent, no changes will be made.
+
+        Arguments:
+            herbie_data: The list containing the Herbie objects
+            extent: The bounding box with the shape: (a, b, c, d):
+                - a: bottom longitude
+                - b: top longitude
+                - c: bottom latitude
+                - d: top latitude
+            extent_name: Desired name of the subregion
+            product: Regex of the product that was downloaded
+
+        Returns:
+            A list of the subregioned grib files
+        '''
         subregion_grib_files = []
         for H in herbie_data:
             # subregion grib files
@@ -178,16 +231,16 @@ class HRRRData:
 
         return subregion_grib_files
 
-    '''
-    Converts a list of downloaded grib files into numpy
-
-    Arguments:
-        grib_files: The paths of the grib files being converted
-
-    Returns:
-        A numpy array of the grib files
-    '''
     def __grib_to_np(self, grib_files):
+        '''
+        Converts a list of downloaded grib files into numpy
+
+        Arguments:
+            grib_files: The paths of the grib files being converted
+
+        Returns:
+            A numpy array of the grib files
+        '''
         np_of_grib = []
         for file in grib_files:
             hrrr_xarr = xr.open_dataset(file, engine="cfgrib", decode_timedelta=False)
@@ -208,18 +261,18 @@ class HRRRData:
 
         return np.array(np_of_grib)
 
-    ''' 
-    Takes a dataset frames (x, y), interpolates/decimates according to
-    the dimensions, and adds a channel axis.
-
-    Arguments:
-        subregion_ds: The numpy array containing a list of frames
-        dim: The desired dimensions
-
-    Returns:
-        A numpy array of frames
-    '''
     def __interpolate_and_add_channel_axis(self, subregion_ds, dim):
+        ''' 
+        Takes a dataset frames (x, y), interpolates/decimates according to
+        the dimensions, and adds a channel axis.
+
+        Arguments:
+            subregion_ds: The numpy array containing a list of frames
+            dim: The desired dimensions
+
+        Returns:
+            A numpy array of frames
+        '''
         n_frames = len(subregion_ds)
         channels = 1
         frames = np.empty(shape=(n_frames, dim, dim, channels))
@@ -232,33 +285,33 @@ class HRRRData:
 
         return frames
 
-    '''
-    Uses a sliding window to create samples from frames.
-
-    For example, with a step size of 5, with 15 frames and a 5 frames per 
-    sample, frames: 
-        - 1-5
-        - 6-10
-        - 11-15
-    will make up a total of 3 samples.
-
-    For example, with a step size of 1, with 5 frames and a 3 frames per 
-    sample, frames: 
-        - 1-3 
-        - 2-4
-        - 3-5 
-    will make up a total of 3 samples.
-
-    Arguments:
-        frames: A numpy array of the shape (num_frames, row, col, channels)
-        window_size: The desired number of frames per sample
-        full_slide: Whether the window should slide by 1 (false) or by the size
-            of the window (true)
-
-    Returns:
-        A numpy array of the shape (num_samples, num_frames, row, col, channels)
-    '''
     def __sliding_window_of(self, frames, window_size, full_slide=False):
+        '''
+        Uses a sliding window to create samples from frames.
+
+        For example, with a step size of 5, with 15 frames and a 5 frames per 
+        sample, frames: 
+            - 1-5
+            - 6-10
+            - 11-15
+        will make up a total of 3 samples.
+
+        For example, with a step size of 1, with 5 frames and a 3 frames per 
+        sample, frames: 
+            - 1-3 
+            - 2-4
+            - 3-5 
+        will make up a total of 3 samples.
+
+        Arguments:
+            frames: A numpy array of the shape (num_frames, row, col, channels)
+            window_size: The desired number of frames per sample
+            full_slide: Whether the window should slide by 1 (false) or by the size
+                of the window (true)
+
+        Returns:
+            A numpy array of the shape (num_samples, num_frames, row, col, channels)
+        '''
         n_frames, row, col, channels = frames.shape
         n_samples = (
             n_frames - window_size + 1
