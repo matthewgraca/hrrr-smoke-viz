92fa1cd9364525e32b666ebe2b3780bcde314c9a
Matthew Graca <matthew.graca@gmail.com>
2025-08-24 17:09:56 -0700

    ancillary files

--- openaq/request.sh
+++ openaq/request.sh
@@ -2,4 +2,5 @@
 # LA Extent + PM2.5 sensors + 1000 hits per page
 curl --request GET \
   --url "https://api.openaq.org/v3/locations?bbox=-118.75,33.5,-117.0,34.5&parameters_id=2&limit=1000" \
-  --header "X-API-Key: $OPENAQ_API_KEY" > response.json
+  -s -D headers.txt -o response.json \
+  --header "X-API-Key: $OPENAQ_API_KEY"
