93433ae4994fc9748803a5b9eaf1c29468e5ddd1
Matthew Graca <matthew.graca@gmail.com>
2025-11-03 17:01:59 -0800

    skeleton of naqfc data and tests

---
+++ libs/naqfcdata.py
@@ -0,0 +1,12 @@
+import numpy as np
+from tqdm import tqdm
+
+class NAQFCData:
+    def __init__(
+        self,
+        start_date="2025-01-10 00:00",
+        end_date="2025-01-10 00:59",
+        extent=(-118.75, -117.0, 33.5, 34.5),
+        dim=40,
+    ):
+        pass
---
+++ tests/naqfcdata/test_naqfcdata.py
@@ -0,0 +1,7 @@
+import unittest
+from libs.naqfcdata import NAQFCData 
+
+class TestNAQFCData(unittest.TestCase):
+    def test(self):
+        nd = NAQFCData()
+        self.assertTrue(True)
