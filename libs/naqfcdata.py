4837cec1c93cf47cb619b8379689638c6aa2ab19
Matthew Graca <matthew.graca@gmail.com>
2025-11-04 19:10:38 -0800

    investigated downloading and some viz

--- libs/naqfcdata.py
+++ libs/naqfcdata.py
@@ -1,5 +1,7 @@
 import s3fs
-import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 class NAQFCData:
     def __init__(
@@ -16,13 +18,40 @@ class NAQFCData:
         print([filename for filename in s3.ls('noaa-nws-naqfc-pds/AQMv7/CS/20240514/06') if 'ave_1hr_pm25_bc' in filename])
 
         # filepath structure
-        # noaa-nws-naqfc-pds/{model}/{area}/{date}/{forecast_start}
+        # noaa-nws-naqfc-pds/AQMv7/CS/20240514/06/aqm.t06z.ave_1hr_pm25_bc.20240514.227.grib2
+        # noaa-nws-naqfc-pds/{model}/{area}/{date}/{forecast_start}/aqm.t{forecast_start}z.ave_1hr_pm25_bc.{date}.{location}.grib2
+
+        # here we can define the valid params
+        area_code = {
+            'AK' : 198,
+            'CS' : 227,
+            'HI' : 196
+        }
         aqm_file_params = {
             'model' : {'AQMv5', 'AQMv6', 'AQMv7', 'AQMv7_suppl'},
             'area' : {'AK', 'CS', 'HI'},
             'forecast_start' : {'06', '12'}
         }
+
+        # then construct a valid set of params
+        path_params = {
+            'model' : 'AQMv7',
+            'area' : 'CS',
+            'forecast_start' : '06',
+            'date' : '20240514'
+        }
         print(aqm_file_params)
+        path = '/'.join([
+            f'noaa-nws-naqfc-pds',
+            f'{path_params["model"]}',
+            f'{path_params["area"]}',
+            f'{path_params["date"]}',
+            f'{path_params["forecast_start"]}',
+            f'aqm.t{path_params["forecast_start"]}z.ave_1hr_pm25_bc.{path_params["date"]}.{area_code[path_params["area"]]}.grib2'
+        ])
+        print(path)
+        src = s3.ls(path)[0]
+        print(src)
         '''
         broadly speaking, there are 3 classes
         1. pm2.5 vs ozone
@@ -35,3 +64,23 @@ class NAQFCData:
         So we want: 'ave_1hr_pm25_bc'
         '''
 
+        # open and read data
+        '''
+        s3.get(src, '/home/mgraca/Workspace/hrrr-smoke-viz')
+        '''
+        # does not like with open() for some reason, some io buffer subscripting error
+        ds = xr.load_dataset('/home/mgraca/Workspace/hrrr-smoke-viz/tests/naqfcdata/data/aqm.t06z.ave_1hr_pm25_bc.20240514.227.grib2', engine='cfgrib')
+    
+        print(ds)
+        '''
+        Probably gonna be s3.open(...) with xarray open/load dataset with cfgrib engine
+        Look into backend_kwargs to see if you can filter things by keys to trim the file
+        with s3.open(s3.ls(path)[0], 'rb') as f:
+            print(f'opening {f}...')
+            ds = xr.open_dataset(f, engine='cfgrib')
+            print('complete.')
+            print(ds.info())
+        '''
+
+
+        # plottingz -> see jupyter notebook
