8623b81315a1f4b86047bc4ff49f5c52d9fbe43a
Matthew Graca <matthew.graca@gmail.com>
2025-12-03 12:35:24 -0800

    created training scripts from base principles, w/ results and manual loss

---
+++ pwwb-experiments/tensorflow/basic-cnn-archive/training.py
@@ -0,0 +1,347 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import keras
+tf.keras.backend.set_image_data_format('channels_last')
+
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product
+from sklearn.metrics import root_mean_squared_error
+
+BASE_PATH = '/home/mgraca/Workspace/hrrr-smoke-viz'
+DATA_PATH = os.path.join(BASE_PATH, 'pwwb-experiments/tensorflow/basic-cnn-archive/training-data')
+RESULTS_PATH = os.path.join(BASE_PATH, 'pwwb-experiments/tensorflow/basic-cnn-archive/results')
+# hardcoded since we already processed AirNowData
+SENSORS = {
+    'Reseda' : (8, 3),
+    'North Holywood' : (8, 11),
+    'Los Angeles - N. Main Street' : (15, 16),
+    'Compton' : (23, 17),
+    'Long Beach Signal Hill' : (29, 19),
+    'Anaheim' : (27, 29),
+    'Glendora - Laurel' : (10, 33)
+}
+AIRNOW_CHANNEL = 11 # NOTE i hate this hardcoding
+
+class TextColor:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'  # Resets formatting
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class TrainingPipeline():
+    def __init__(
+        self,
+        epochs=100,
+        batch_size=64,
+    ):
+        print('Loading default parameters:')
+        print(f'\tPath of the training data: {DATA_PATH}')
+        print(f'\tPath of the results: {RESULTS_PATH}\n')
+        print(f'Airnow sensor locations in use ({len(SENSORS)}):')
+        for k, v in SENSORS.items():
+            print(f'\t{k} : {v}')
+        print()
+
+        X_train, X_valid, X_test, Y_train, Y_valid, Y_test = self._load_data(DATA_PATH)
+        dim = X_train.shape[1]
+        input_shape = X_train.shape[1:]
+
+        model = self._base_model(input_shape)
+
+        print(f'{TextColor.BLUE}Beginning Training{TextColor.ENDC}')
+        history = model.fit(
+            X_train,
+            Y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(X_valid, Y_valid)
+        )
+        print()
+
+        print(f'{TextColor.BLUE}Beginning Inference{TextColor.ENDC}')
+        y_pred = self._evaluate(model, SENSORS, dim, X_test, Y_test)
+        print()
+
+        print(f'{TextColor.BLUE}Saving results{TextColor.ENDC}')
+        self._plot_loss_curves(history)
+        self._plot_predictions(X_test, Y_test, y_pred)
+        np.save(os.path.join(RESULTS_PATH, 'y_pred.npy'), y_pred)
+
+        return
+
+    def _load_data(self, DATA_PATH):
+        print('Loading training data (eager loading)...', end=' ')
+        X_train = np.load(os.path.join(DATA_PATH, 'X_train.npy'))
+        X_valid = np.load(os.path.join(DATA_PATH, 'X_valid.npy'))
+        X_test = np.load(os.path.join(DATA_PATH, 'X_test.npy'))
+        Y_train = np.load(os.path.join(DATA_PATH, 'Y_train.npy'))
+        Y_valid = np.load(os.path.join(DATA_PATH, 'Y_valid.npy'))
+        Y_test = np.load(os.path.join(DATA_PATH, 'Y_test.npy'))
+        print('complete!\n')
+
+        # initially fit data for conv3d, not conv2d; requires us to squeeze out the frame dimension
+        # (sample, frames, h, w, c) -> (sample, h, w, c)
+        X_train = np.squeeze(X_train)
+        X_valid = np.squeeze(X_valid)
+        X_test = np.squeeze(X_test)
+        Y_train = np.squeeze(Y_train)
+        Y_valid = np.squeeze(Y_valid)
+        Y_test = np.squeeze(Y_test)
+
+        train_usage = X_train.nbytes + Y_train.nbytes
+        valid_usage = X_valid.nbytes + Y_valid.nbytes
+        test_usage = X_test.nbytes + Y_test.nbytes
+
+        print('Datasets shape')
+        print(f'\tTrain: X={X_train.shape}, Y={Y_train.shape}, Space usage: {train_usage/1e9:.2f}GB')
+        print(f'\tValid: X={X_valid.shape}, Y={Y_valid.shape}, Space usage: {valid_usage/1e9:.2f}GB')
+        print(f'\tTest : X={X_test.shape}, Y={Y_test.shape}, Space usage: {test_usage/1e9:.2f}GB')
+        print(f'Total usage: {(train_usage + valid_usage + test_usage) / 1e9:.2f}GB\n')
+
+        return X_train, X_valid, X_test, Y_train, Y_valid, Y_test
+
+    def _evaluate(self, model, sensors, dim, X_test, Y_test):
+        Y_pred = model.predict(X_test)
+
+        # note that this is the same nhood loss as the training. so sensor location + nhood + background 
+        # are accounted for; this is not just a raw nhood loss only.
+        nhood_loss = self.NHoodLoss(sensors, dim)(Y_test, Y_pred).numpy()
+        print(f'Neighborhood loss: {nhood_loss}')
+        sensor_loss = self.SensorLoss(sensors)(Y_test, Y_pred).numpy()
+        print(f'Direct sensor loss: {sensor_loss}')
+        return Y_pred
+        
+        '''
+        frame_metrics = {}
+        for hour in range(horizon):
+            y_true_h = Y_test_sensors[:, hour, :].flatten()
+            y_pred_h = Y_pred_sensors[:, hour, :].flatten()
+            
+            hour_rmse = root_mean_squared_error(y_true_h, y_pred_h))
+            hour_nrmse = (hour_rmse / np.mean(y_true_h)) * 100
+            
+            frame_metrics[hour+1] = {
+                'rmse': hour_rmse,
+                'nrmse': hour_nrmse
+            }
+        
+        avg_nrmse = np.mean([frame_metrics[h]['nrmse'] for h in frame_metrics])
+        
+        print(f"\nTest Set Metrics:")
+        print(f"  Average NRMSE: {avg_nrmse:.2f}%")
+        
+        print(f"\nHourly NRMSE:")
+        for h in range(1, horizon + 1):
+            print(f"  Hour {h:2d}: {frame_metrics[h]['nrmse']:.2f}%")
+        
+        return {
+            'avg_nrmse': avg_nrmse,
+            'predictions': Y_pred,
+            'frame_metrics': frame_metrics
+        }
+        '''
+
+    ### NOTE visualizations (things that will be saved to results/)
+
+    def _plot_predictions(self, X_test, Y_test, y_pred, save_path=RESULTS_PATH):
+        np.random.seed(42)
+        fig, axes = plt.subplots(nrows=3, ncols=5, figsize=(12, 8))
+
+        # not directly plotting X_test since scaling affects visualization
+        for c in range(axes.shape[1]):
+            idx = np.random.randint(1, len(Y_test))
+            vmin = np.nanmin([Y_test[idx], y_pred[idx], Y_test[idx-1]])
+            vmax = np.nanmax([Y_test[idx], y_pred[idx], Y_test[idx-1]])
+            
+            axes[0, c].imshow(Y_test[idx-1], vmin=vmin, vmax=vmax)
+            axes[0, c].set_title(f'index: {idx}')
+            axes[0, c].set_xticks([])
+            axes[0, c].set_yticks([])
+            
+            axes[1, c].imshow(Y_test[idx], vmin=vmin, vmax=vmax)
+            axes[1, c].set_xticks([])
+            axes[1, c].set_yticks([])
+
+            im = axes[2, c].imshow(y_pred[idx], vmin=vmin, vmax=vmax)
+            axes[2, c].set_xticks([])
+            axes[2, c].set_yticks([])
+
+            fig.colorbar(im, ax=axes[:, c], orientation='horizontal', fraction=0.046, pad=0.04)
+            
+        fig.text(0.11, 0.775, 'Test Input', rotation=90, va='center', ha='center', fontsize=14)
+        fig.text(0.11, 0.525, 'Test Target', rotation=90, va='center', ha='center', fontsize=14)
+        fig.text(0.11, 0.275, 'Prediction', rotation=90, va='center', ha='center', fontsize=14)
+        plt.savefig(os.path.join(RESULTS_PATH, 'pred_vs_test_samples.png'))
+
+        return
+
+    def _plot_loss_curves(self, history, save_path=RESULTS_PATH):
+        plt.figure(figsize=(10, 6))
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.legend()
+        plt.title(f'\nTraining Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss (MAE)')
+        plt.grid(True, alpha=0.3)
+        plt.savefig(os.path.join(save_path, 'loss_curve.png'))
+
+        return
+
+    ### NOTE models
+
+    def _base_model(self, input_shape):
+        from keras.models import Sequential
+        from keras.layers import Conv2D, Input, Reshape
+        from keras.optimizers import Adam
+
+        model = Sequential()
+        model.add(Input(shape=input_shape))
+        model.add(Conv2D(filters=15, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(filters=30, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(filters=15, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(Conv2D(filters=1, kernel_size=(3, 3), activation='relu', padding='same'))
+        model.add(Reshape((input_shape[0], input_shape[1])))
+
+        model.compile(
+            loss=self.NHoodLoss(
+                sensors=SENSORS,
+                dim=input_shape[1],
+                #source_weight,
+                #nhood_weight,
+                #r
+            ),
+            optimizer=Adam()
+        )
+
+        model.summary()
+        print()
+
+        return model
+
+    class SensorLoss(tf.keras.losses.Loss):
+        def __init__(self, sensors):
+            super().__init__()
+            self.sensors = list(sensors.values())
+
+        def call(self, y_true, y_pred):
+            errors = [
+                tf.abs(tf.subtract(y_true[:, x, y], y_pred[:, x, y]))
+                for x, y in self.sensors
+            ]
+            return tf.reduce_mean(tf.stack(errors, axis=-1))
+
+        # for saving and loading models using this loss
+        def get_config(self):
+            config = super().get_config()
+            config.update({'sensors': self.sensors})
+            return config
+
+    class NHoodLoss(tf.keras.losses.Loss):
+        def __init__(self, sensors, dim, source_weight=25, nhood_weight=5, bg_weight=1, r=2):
+            super().__init__()
+            self.weights = self._get_weights(
+                list(sensors.values()),
+                dim,
+                source_multiplier=source_weight,
+                n_hood_multiplier=nhood_weight,
+                bg_multiplier=bg_weight,
+                radius=r
+            )
+
+        def call(self, y_true, y_pred):
+            return tf.reduce_mean(tf.abs(y_true - y_pred) * self.weights)
+
+        #### NOTE long block of funcs that find weights
+        def _in_bounds(self, x, y, bound):
+            '''
+            ensures that neighborhood pair that is outside the dimensions doesn't
+                get counted
+            '''
+            x_in_bound = x >= 0 and x < bound
+            y_in_bound = y >= 0 and y < bound
+
+            return x_in_bound and y_in_bound
+        
+        def _find_neighbors(self, sources, radius, dim):
+            '''
+            Finds the (x, y) pairs that serve as the neighbors of the sources
+            '''
+            n_hood = set(product(range(-radius, radius + 1), repeat=2))
+            n_hood.remove((0, 0))
+            neighbors = set()
+            for x, y in sources:
+                for a, b in n_hood:
+                    f, g = x + a, y + b
+                    if self._in_bounds(f, g, dim):
+                        neighbors.add((f, g))
+
+            return neighbors
+
+        def _determine_weights(
+            self,
+            sources,
+            n_hood,
+            dim,
+            source_multiplier,
+            n_hood_multiplier,
+            bg_multiplier
+        ):
+            '''
+            applies the proper weights to:
+                - the background
+                - the neighborhoods
+                - the sources
+            based on the list of pairs in sources and nhood
+            '''
+            weights = np.full((dim, dim), bg_multiplier)
+            for (x, y) in n_hood:
+                weights[x, y] = n_hood_multiplier
+            for (x, y) in sources:
+                weights[x, y] = source_multiplier 
+
+            return weights
+        
+        def _get_weights(
+            self,
+            sensor_locations,
+            dim,
+            source_multiplier=25,
+            n_hood_multiplier=5,
+            bg_multiplier=1,
+            radius=1
+        ):
+            sensor_coords = set(sensor_locations)
+            neighbors = self._find_neighbors(sensor_coords, radius, dim)
+            weights = self._determine_weights(
+                sensor_coords,
+                neighbors,
+                dim,
+                source_multiplier,
+                n_hood_multiplier,
+                bg_multiplier
+            )
+            return weights
+
+        # for saving and loading models using this loss
+        def get_config(self):
+            config = super().get_config()
+            config.update({'sensors': self.sensors})
+            return config
+
+def main():
+    TrainingPipeline(
+        epochs=10,
+        batch_size=64,
+    )
+
+if __name__ == "__main__":
+    main()
