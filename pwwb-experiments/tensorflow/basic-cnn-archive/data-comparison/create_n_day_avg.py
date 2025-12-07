f6c92978a1a2b978b06f0e470c55d8b9c732e620
Matthew Graca <matthew.graca@gmail.com>
2025-12-07 12:15:22 -0800

    clean up + added cmd line args for dynamically setting n day lookback

--- pwwb-experiments/tensorflow/basic-cnn-archive/data-comparison/create_n_day_avg.py
+++ pwwb-experiments/tensorflow/basic-cnn-archive/data-comparison/create_n_day_avg.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import root_mean_squared_error as rmse
 from sklearn.metrics import mean_absolute_error as mae 
+import sys
 
 BASE_PATH = '/home/mgraca/Workspace/hrrr-smoke-viz/pwwb-experiments/tensorflow/basic-cnn-archive/'
 DATA_PATH = os.path.join(BASE_PATH, 'processing-scripts/l2')
@@ -14,30 +15,43 @@ dates = pd.date_range('2023-08-02', '2025-08-02', inclusive='left', freq='h')
 idx_of_date = {date : i for i, date in enumerate(dates)}
 date_of_idx = {i : date for i, date in enumerate(dates)}
 
-def thirty_day_lookback_avg_of(idx, frames):
+n = 0
+if len(sys.argv) == 1:
+    n = 30
+    print('Defaulting to 30-day lookback')
+elif len(sys.argv) == 2:
+    try:
+        n = int(sys.argv[1])
+    except:
+        sys.exit('int arg pls')
+    print(f'Using {n}-day lookback')
+else:
+    sys.exit('One int arg pls')
+
+def n_day_lookback_avg_of(n, idx, frames):
     '''
     Caveat: produces empty list if there is no previous frame to lookback
 
     Results in the first 24 frames being truncated.
     '''
-    i = 1
+    i = 0
     prev_day_idx = idx - 24
-    thirty_days_frames = []
-    while i < 30 and prev_day_idx >= 0:
-        thirty_days_frames.append(frames[prev_day_idx])
+    n_days_frames = []
+    while i < n and prev_day_idx >= 0:
+        n_days_frames.append(frames[prev_day_idx])
         prev_day_idx -= 24
         i += 1
 
     return (
-        np.mean(np.array(thirty_days_frames), axis=0)
-        if len(thirty_days_frames) != 0
+        np.mean(np.array(n_days_frames), axis=0)
+        if len(n_days_frames) != 0
         else np.array([])
     )
 
 # generate 30-day avg frames
 res = []
 for i in range(airnow.shape[0]):
-    arr = thirty_day_lookback_avg_of(i, airnow)
+    arr = n_day_lookback_avg_of(n, i, airnow)
     if len(arr != 0):
         res.append(arr)
 res = np.array(res)
@@ -54,7 +68,7 @@ vmax = np.nanmax([airnow[idx], res[idx]])
 axes[0].imshow(airnow[idx+24], vmin=vmin, vmax=vmax)
 axes[0].set_title('Airnow')
 axes[1].imshow(res[idx], vmin=vmin, vmax=vmax)
-axes[1].set_title('30-day average')
+axes[1].set_title(f'{n}-day average')
 plt.suptitle(f'Time: {date_of_idx[idx+24]}')
 
 plt.show()
@@ -81,6 +95,15 @@ print(f'RMSE: {rmse_sensor:.2f}')
 print(f'NRMSE: {nrmse_sensor:.2f}%')
 
 # viz (use matplotlib for the viz, zooming in really helps)
-plt.plot(dates[24:], np.mean(realigned_airnow[:, X, Y], axis=-1))
-plt.plot(dates[24:], np.mean(res[:, X, Y], axis=-1))
+plt.plot(dates[24:], np.mean(realigned_airnow[:, X, Y], axis=-1), label='AirNow')
+plt.plot(dates[24:], np.mean(res[:, X, Y], axis=-1), label='Lookback Avg')
+plt.legend()
+plt.title(f'AirNow vs {n}-day lookback average: All sensors')
+plt.show()
+
+# example la sensor
+plt.plot(dates[24:], realigned_airnow[:, 15, 16], label='AirNow')
+plt.plot(dates[24:], res[:, 15, 16], label='Lookback Avg')
+plt.legend()
+plt.title(f'AirNow vs {n}-day lookback average: LA sensor')
 plt.show()
