f678cff6e226df045c9bbdedb6074c928b6f6318
Matthew Graca <matthew.graca@gmail.com>
2026-01-09 18:21:19 -0800

    pulled out models into libs

--- pwwb-experiments/tensorflow/autoencoder_archive/training.py
+++ pwwb-experiments/tensorflow/autoencoder_archive/training.py
@@ -1,12 +1,13 @@
 import argparse
 
-valid_models = set(['classic', 'two_path'])
+valid_models = set(['classic', 'two_path', 'dual_autoencoder'])
 valid_losses = set(['grid', 'nhood'])
 
 parser = argparse.ArgumentParser(description='Training script (you will need to edit paths in the script!)')
 parser.add_argument('model', help=f'the model to train: {valid_models}')
 parser.add_argument('loss', help=f'the loss function to use: {valid_losses}')
 parser.add_argument('-t', '--test', action='store_true', help='triggers a test instead of a full run (saved in test_experiment, epochs set to 1)')
+parser.add_argument('-b', '--build', action='store_true', help='only builds the model; does not trigger training')
 args = parser.parse_args()
 
 # training parameters
@@ -33,17 +34,19 @@ import sys
 EXPERIMENT_NAME = 'test_experiment' if args.test else MODEL_NAME + '_model_' + LOSS_NAME + '_loss'
 
 BASE_PATH = '/home/mgraca/Workspace/hrrr-smoke-viz'
-EXPERIMENT_PATH = os.path.join(BASE_PATH, 'pwwb-experiments/tensorflow/runback')
+EXPERIMENT_PATH = os.path.join(BASE_PATH, 'pwwb-experiments/tensorflow/autoencoder_archive')
 DATA_PATH = os.path.join(EXPERIMENT_PATH, 'preprocessed_cache/npy_files')
 RESULTS_PATH = os.path.join(EXPERIMENT_PATH, f'results/{EXPERIMENT_NAME}')
 os.makedirs(RESULTS_PATH, exist_ok=True)
 
 sys.path.append(BASE_PATH)
 from libs.pwwb.utils.dataset import PWWBPyDataset
+import libs.models as model
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+from tensorflow.keras.utils import plot_model
 
 class TextColor:
     HEADER = '\033[95m'
@@ -66,68 +69,6 @@ with open(os.path.join(EXPERIMENT_PATH, 'preprocessed_cache/metadata.pkl'), 'rb'
     metadata = pickle.load(f)
 channel_to_idx = {ch : i for i, ch in enumerate(metadata['channel_names'])}
 
-# common args in each layer to reduce function call size
-CONVLSTM2D_ARGS = {
-    'kernel_size': (3, 3),
-    'padding': 'same',
-    'return_sequences': True
-}
-
-CONV3D_ARGS = {
-    'kernel_size': (3, 3, 3),
-    'activation': 'relu',
-    'padding': 'same'    
-}
-
-# model definition
-def classic_model(input_shape):
-    from keras.models import Sequential
-    from keras.layers import ConvLSTM2D, Conv3D, InputLayer
-    from keras.optimizers import Adam
-
-    # model definition
-    model = Sequential()
-    model.add(InputLayer(input_shape))
-    model.add(ConvLSTM2D(filters=25, **CONVLSTM2D_ARGS))
-    model.add(ConvLSTM2D(filters=50, **CONVLSTM2D_ARGS))
-    model.add(Conv3D(filters=25, **CONV3D_ARGS))
-    model.add(Conv3D(filters=1, **CONV3D_ARGS))
-
-    return model
-
-def two_path_model(input_shape, path1_channels, path2_channels):
-    # pass in the indices of the channels, not their name!
-    from keras.layers import Input, Conv3D, ConvLSTM2D
-    from keras.layers import Concatenate
-    from keras.models import Model
-    from libs.layers import ChannelSplitLayer
-
-    def path_block(x, name):
-        x = ConvLSTM2D(filters=20, **CONVLSTM2D_ARGS, name=f'{name}_convlstm_2d_1')(x)
-        x = ConvLSTM2D(filters=40, **CONVLSTM2D_ARGS, name=f'{name}_convlstm_2d_2')(x)
-        x = Conv3D(filters=20, **CONV3D_ARGS, name=f'{name}_conv3d_1')(x)
-        return x
-
-    def trunk_block(x, name):
-        x = Conv3D(filters=30, **CONV3D_ARGS, name=f'{name}_conv3d_1')(x)
-        x = Conv3D(filters=20, **CONV3D_ARGS, name=f'{name}_conv3d_2')(x)
-        x = Conv3D(filters=1, **CONV3D_ARGS, name=f'{name}_conv3d_3')(x)
-        return x
-
-    inputs = Input(shape=input_shape)
-
-    p_1 = ChannelSplitLayer(path1_channels)(inputs)
-    p_1 = path_block(p_1, name='pm25_path')
-
-    p_2 = ChannelSplitLayer(path2_channels)(inputs)
-    p_2 = path_block(p_2, name='other_path')
-
-    x = Concatenate(axis=-1)([p_1, p_2])
-    x = trunk_block(x, name='trunk')
-
-    model = Model(inputs, x, name='two_path_model')
-
-    return model
 
 # load dataset
 print(f'{TextColor.BLUE}Lazy loading datasets...{TextColor.ENDC}')
@@ -165,7 +106,7 @@ input_shape = train_ds.input_shape
 f, h, w, c = input_shape
 match MODEL_NAME:
     case 'classic':
-        model = classic_model(input_shape)
+        model = model.classic(input_shape)
     case 'two_path':
         path1_channel_names = [
             'AirNow_PM25', 'AirNow_Hourly_Clim', 'OpenAQ_PM25', 'NAQFC_PM25',
@@ -176,10 +117,40 @@ match MODEL_NAME:
             'HRRR_Wind_U', 'HRRR_Wind_V', 'HRRR_Wind_Speed', 'HRRR_Temp_2m',
             'HRRR_PBL_Height', 'HRRR_Precip_Rate', 'Elevation', 'NDVI', 'GOES', 'TEMPO'
         ]
-        model = two_path_model(
+        model = model.two_path(
             input_shape,
             path1_channels=[channel_to_idx[ch] for ch in path1_channel_names],
             path2_channels=[channel_to_idx[ch] for ch in path2_channel_names],
+        )
+    case 'dual_autoencoder':
+        ARCH_5x5 = {
+            'temporal_filters': [16, 24, 32],
+            'spatial_filters': [32, 48, 64],
+            'bottleneck': 128,
+            'latent_size': 5,
+            'strides': [2, 2, 2]
+        }
+
+        ARCH_10x10 = {
+            'temporal_filters': [16, 24, 32],
+            'spatial_filters': [32, 48],
+            'bottleneck': 256,
+            'latent_size': 10,
+            'strides': [2, 2]
+        }
+
+        model = model.dual_autoencoder(
+            input_shape,
+            arch_config=ARCH_5x5,
+            output_horizon=metadata['forecast_horizon'],
+            observed_channels=[
+                metadata['channel_names'].index(ch)
+                for ch in metadata['observed_channels']
+            ],
+            forecast_channels=[
+                metadata['channel_names'].index(ch)
+                for ch in metadata['forecast_channels']
+            ]
         )
     case _:
         raise ValueError(f'Invalid model choice; pick from : {valid_models}')
@@ -195,6 +166,19 @@ match LOSS_NAME:
 
 model.summary()
 print()
+
+plot_model(
+    model, 
+    to_file=os.path.join(RESULTS_PATH, 'model.png'),
+    show_layer_names=True, 
+    expand_nested=True,
+    show_shapes=True,
+    rankdir='TB' # TB=vertical, LR=horizontal
+)
+
+if args.build:
+    print(f'{TextColor.BLUE}Build mode activated, skipping training.{TextColor.ENDC}')
+    sys.exit()
 
 print(f'{TextColor.BLUE}Beginning Training{TextColor.ENDC}')
 history = model.fit(
