2157ea355c4b072222fd4cce07baffa9e524daaf
Matthew Graca <matthew.graca@gmail.com>
2026-01-14 15:59:41 -0800

    mass refactor; added enc_tile back

--- libs/models.py
+++ libs/models.py
@@ -19,7 +19,6 @@ def classic(input_shape):
         'padding': 'same'    
     }
 
-    # model definition
     model = Sequential()
     model.add(InputLayer(input_shape))
     model.add(ConvLSTM2D(filters=25, **CONVLSTM2D_ARGS))
@@ -79,10 +78,12 @@ def two_path(input_shape, path1_channels, path2_channels):
 def dual_autoencoder(
     input_shape, 
     arch_config, 
+    convlstm_reg_config,
     output_horizon, 
     observed_channels, 
     forecast_channels
 ):
+    import tensorflow as tf
     from tensorflow.keras.layers import (
         Input, Conv2D, Conv3D, ConvLSTM2D, Dense, Flatten, 
         Reshape, UpSampling2D, Lambda, concatenate,
@@ -91,108 +92,252 @@ def dual_autoencoder(
     from tensorflow.keras.models import Model
     from libs.layers import ChannelSplitLayer
 
+    def spatial_decoder(x, past_encs, fcast_encs, spatial_filters, temporal_filters):
+        '''
+        Super tough to wrap this in a Model since tf really doesn't like the passing of 
+        past_encs and fcast_encs, they'll need to be their own Input, etc.
+
+        If the goal is a cleaner plot_model(), the juice is not worth the squeeze
+        '''
+        skips = list(zip(reversed(past_encs[:-1]), reversed(fcast_encs[:-1])))
+        for i, (past_skip, fcast_skip) in enumerate(skips):
+            x = UpSampling2D(
+                size=(2,2), 
+                interpolation='bilinear', 
+                name=f'dec_up{i+1}'
+            )(x)
+            x = Conv2D(
+                filters=spatial_filters[-(i+2)],
+                kernel_size=(3,3), 
+                padding='same', 
+                activation='relu', 
+                name=f'dec_d{i+1}'
+            )(x)
+            x = Add(name=f'e_{2-i}_skip')([x, past_skip, fcast_skip])
+        x = UpSampling2D(size=(2,2), interpolation='bilinear', name=f'dec_up{len(spatial_filters)}')(x)
+        x = Conv2D(temporal_filters[2], (3,3), padding='same', activation='relu', name=f'dec_d{len(spatial_filters)}')(x)
+        return x
+
+    def arm_block(
+        input_shape, 
+        filters, 
+        forecast_arm=False
+    ):
+        '''
+        Two return options:
+            - forecast_arm false: uses past arm architecture,
+                returns hidden and cell state for skip connection
+            - forecast_arm true: uses future arm architecture, 
+                no hidden/cell state returned
+        '''
+        _b, t, h, w, c = input_shape
+        prefix = 'fcast' if forecast_arm else 'past'
+
+        # block 1
+        inputs = Input(shape=(t, h, w, c), name=f'{prefix}_inputs')
+        x_1 = Conv3D(
+            filters=filters[0], 
+            kernel_size=(1,1,1),
+            padding='same', 
+            name=f'{prefix}_proj'
+        )(inputs)
+        x_2 = ConvLSTM2D(
+            filters=filters[0], 
+            kernel_size=(3,3),
+            padding='same', 
+            return_sequences=True,
+            name=f'{prefix}_lstm1'
+        )(x_1)
+        x_out = Add(name=f'skip_{prefix}_lstm1')([x_2, x_1])
+
+        # block 2
+        y_1 = ConvLSTM2D(
+            filters=filters[1],
+            kernel_size=(3,3),
+            padding='same',
+            return_sequences=True,
+            name=f'{prefix}_lstm2'
+        )(x_out)
+        y_2 = Conv3D(
+            filters=filters[1],
+            kernel_size=(1,1,1),
+            padding='same', 
+            name=f'{prefix}_res1_proj'
+        )(x_out)
+        y_out = Add(name=f'skip_{prefix}_res1_proj')([y_1, y_2])
+
+        # block 3
+        # past arm supports a 3rd convlstm layer that returns its hidden and cell state
+        # also expands filters
+        out, hidden_state, cell_state = (
+            (y_out, None, None)
+            if forecast_arm
+            else ConvLSTM2D(
+                filters=filters[2],
+                kernel_size=(3,3),
+                padding='same', 
+                return_sequences=True,
+                return_state=True, 
+                name=f'{prefix}_lstm3'
+            )(y_out)
+        )
+
+        return Model(
+            inputs, 
+            outputs=out if forecast_arm else [out, hidden_state, cell_state],
+            name=f'{prefix}_arm'
+        )
+
+    def encoder_block(input_shape, t_filters, s_filters, strides, prefix):
+        _b, t, h, w, c = input_shape
+        inputs = Input(shape=(t, h, w, c))
+
+        # temporal compression
+        x = Lambda(
+            function=lambda tensor: tf.gather(tensor, indices=tf.shape(tensor)[1] - 1, axis=1),
+            output_shape=(h, w, c),
+            name=f'{prefix}_gather_last_frame'
+        )(inputs)
+        x = Conv2D(
+            filters=t_filters[2],
+            kernel_size=(1,1), 
+            padding='same', 
+            name=f'{prefix}_res2_proj'
+        )(x)
+
+        # spatial compression
+        encs = []
+        for i, filters in enumerate(s_filters):
+            x = Conv2D(
+                filters=filters, 
+                kernel_size=(3,3),
+                strides=strides[i], 
+                padding='same', 
+                activation='relu', 
+                name=f'{prefix}_e{i+1}'
+            )(x)
+            encs.append(x)
+        return Model(inputs, outputs=[x, *encs], name=f'{prefix}_encoder')
+
+    def bottleneck_block(input_shape, latent_size, bottleneck, spatial_filters):
+        # aka latent space fusion
+        _b, h, w, c = input_shape
+        inputs = Input(shape=(h, w, c), name='fused_encodings')
+        
+        x = Flatten(name='bn_flat')(inputs)
+        x = Dense(bottleneck, activation='relu', name='bn_dense')(x)
+        x = Dense(
+            units=latent_size * latent_size * spatial_filters[-1],
+            activation='relu',
+            name='bn_proj'
+        )(x)
+        x = Reshape(
+            target_shape=(latent_size, latent_size, spatial_filters[-1]),
+            name='bn_reshape'
+        )(x)
+    
+        return Model(inputs, outputs=x, name='bottleneck')
+
+    def temporal_decoder(input_shapes, in_t, output_horizon, spatial_filters, temporal_filters):
+        def create_pos_encoding(x, output_horizon):
+            pos = tf.reshape(
+                tensor=tf.range(output_horizon, dtype=tf.float32) / output_horizon,
+                shape=[1, output_horizon, 1, 1, 1]
+            )
+            return tf.tile(
+                input=pos,
+                multiples=[tf.shape(x)[0], 1, tf.shape(x)[2], tf.shape(x)[3], 1]
+            )
+        def tile(x, output_horizon):
+            return tf.tile(
+                input=tf.expand_dims(x, axis=1),
+                multiples=[1, output_horizon, 1, 1, 1]
+            )
+    
+        _b, h, w, _c = input_shapes[0]
+        inputs = Input(shape=input_shapes[0][1:])
+        hidden_state = Input(shape=input_shapes[1][1:], name='past_hidden_skip')
+        cell_state = Input(shape=input_shapes[2][1:], name='past_cell_skip')
+
+        x_1 = Lambda(
+            function=lambda t : tile(t, output_horizon),
+            output_shape=(in_t, w, h, spatial_filters[0]),
+            name='dec_tile'
+        )(inputs)
+        x_2 = Lambda(
+            function=lambda t: create_pos_encoding(t, output_horizon),
+            output_shape=(in_t, w, h, 1),
+            name='pos_enc'
+        )(x_1)
+        y = concatenate([x_1, x_2], axis=-1, name='dec_input')
+        
+        y = ConvLSTM2D(
+            filters=temporal_filters[2],
+            kernel_size=(3,3), 
+            padding='same', 
+            return_sequences=True, 
+            name='dec_lstm1'
+        )(
+            y, initial_state=[hidden_state, cell_state]
+        )
+        y = Add(name='skip_pos_enc_and_dec_lstm1')([y, x_1])
+        
+        z = ConvLSTM2D(
+            filters=temporal_filters[2], 
+            kernel_size=(3,3),
+            padding='same', 
+            return_sequences=True, 
+            name='dec_lstm2'
+        )(y)
+        z = Add(name='skip_dec_lstm2')([z, y])
+
+        return Model(
+            inputs=[inputs, hidden_state, cell_state],
+            outputs=z, 
+            name='temporal_decoder'
+        )
+    
     temporal_filters = arch_config['temporal_filters']
     spatial_filters = arch_config['spatial_filters']
     bottleneck = arch_config['bottleneck']
     latent_size = arch_config['latent_size']
     strides = arch_config['strides']
-    
-    def tile_for_decoder(x):
-        return tf.tile(tf.expand_dims(x, axis=1), [1, output_horizon, 1, 1, 1])
-    
-    def create_pos_encoding(x):
-        batch_size = tf.shape(x)[0]
-        pos = tf.reshape(tf.range(output_horizon, dtype=tf.float32) / output_horizon, [1, output_horizon, 1, 1, 1])
-        return tf.tile(pos, [batch_size, 1, in_h, in_w, 1])
-    
-    def spatial_encoder(x, prefix):
-        encs = []
-        for i, filters in enumerate(spatial_filters):
-            x = Conv2D(filters, (3,3), strides=strides[i], padding='same', activation='relu', name=f'{prefix}_e{i+1}')(x)
-            encs.append(x)
-        return encs
-    
-    def spatial_decoder(x, past_encs, fcast_encs):
-        skips = list(zip(reversed(past_encs[:-1]), reversed(fcast_encs[:-1])))
-        for i, (past_skip, fcast_skip) in enumerate(skips):
-            x = UpSampling2D(size=(2,2), interpolation='bilinear', name=f'dec_up{i+1}')(x)
-            x = Conv2D(spatial_filters[-(i+2)], (3,3), padding='same', activation='relu', name=f'dec_d{i+1}')(x)
-            x = Add()([x ,past_skip, fcast_skip])
-        x = UpSampling2D(size=(2,2), interpolation='bilinear', name=f'dec_up{len(spatial_filters)}')(x)
-        x = Conv2D(temporal_filters[2], (3,3), padding='same', activation='relu', name=f'dec_d{len(spatial_filters)}')(x)
-        return x
 
-    def encoder_arm_block(input_shape, temporal_filters, name='encoder_arm_block'):
-        _b, t, h, w, c = input_shape
-        filters_1, filters_2, filters_3 = temporal_filters
-
-        inputs = Input(shape=(t, h, w, c))
-        x_1 = Conv3D(filters=filters_1, kernel_size=(1,1,1), padding='same', name='past_proj')(inputs)
-        x_2 = ConvLSTM2D(filters=filters_1, kernel_size=(3,3), padding='same', return_sequences=True, name='past_lstm1')(x_1)
-        x_out = Add()([x_2, x_1])
-
-        y_1 = ConvLSTM2D(temporal_filters[1], (3,3), padding='same', return_sequences=True, name='past_lstm2')(x_out)
-        y_2 = Conv3D(temporal_filters[1], (1,1,1), padding='same', name='past_res1_proj')(x_out)
-        y_out = Add()([y_1, y_2])
-
-        z_1, hidden_state, cell_state = ConvLSTM2D(temporal_filters[2], (3,3), padding='same', return_state=True, name='past_lstm3')(y_out)
-        z_2 = Conv2D(temporal_filters[2], (1,1), padding='same', name='past_res2_proj')(
-            Lambda(lambda t: tf.gather(t, indices=tf.shape(t)[1] - 1, axis=1), output_shape=(h, w, c), name='gather_last_frame')(y_out)
-        )
-        out = Add()([z_1, z_2])
-
-        return Model(inputs, outputs=[out, hidden_state, cell_state], name=name)
-
+    in_t, in_h, in_w, in_c = input_shape 
     inputs = Input(shape=input_shape)
-    in_t, in_h, in_w, in_c = input_shape
     
     print(f"Input: {input_shape} -> Output: ({output_horizon}, {in_h}, {in_w}, 1)")
     
-    observed_in = ChannelSplitLayer(observed_channels)(inputs) 
+    # past encoder arm
+    past_in = ChannelSplitLayer(observed_channels, name='past_channels')(inputs)
+    past_state, h, c = arm_block(
+        input_shape=past_in.shape,
+        filters=temporal_filters,
+        forecast_arm=False
+    )(past_in)
+    past_enc, *past_encs = encoder_block(
+        past_state.shape, temporal_filters, spatial_filters, strides, 'past'
+    )(past_state)
 
-    past_state, h, c = encoder_arm_block(
-        input_shape=observed_in.shape,
-        temporal_filters=temporal_filters,
-        name='current_encoder_arm_block'
-    )(observed_in)
+    # fcast encoder arm
+    fcast_in = ChannelSplitLayer(forecast_channels, name='fcast_channels')(inputs)
+    fcast_state = arm_block(
+        input_shape=fcast_in.shape,
+        filters=temporal_filters,
+        forecast_arm=True
+    )(fcast_in)
+    fcast_enc, *fcast_encs = encoder_block(
+        fcast_state.shape, temporal_filters, spatial_filters, strides, 'fcast'
+    )(fcast_state)
     
-    past_encs = spatial_encoder(past_state, 'past')
-    
-    forecast_in = ChannelSplitLayer(forecast_channels)(inputs)
-    fcast_proj = Conv3D(temporal_filters[0], (1,1,1), padding='same', name='fcast_proj')(forecast_in)
-    fcast_lstm1 = ConvLSTM2D(temporal_filters[0], (3,3), padding='same', return_sequences=True, name='fcast_lstm1')(fcast_proj)
-    fcast_lstm1 = Add()([fcast_lstm1, fcast_proj])
-    fcast_lstm2 = ConvLSTM2D(temporal_filters[1], (3,3), padding='same', return_sequences=True, name='fcast_lstm2')(fcast_lstm1)
-    fcast_lstm1_proj = Conv3D(temporal_filters[1], (1,1,1), padding='same', name='fcast_res1_proj')(fcast_lstm1)
-    fcast_lstm2 = Add()([fcast_lstm2, fcast_lstm1_proj])
-    fcast_state = fcast_lstm2[:, -1]
-    fcast_state = Conv2D(temporal_filters[2], (1,1), padding='same', activation='relu', name='fcast_out_proj')(fcast_state)
-    
-    fcast_encs = spatial_encoder(fcast_state, 'fcast')
-    
+    # bottleneck/latent space fusion
     fused = concatenate([past_encs[-1], fcast_encs[-1]], axis=-1, name='bn_concat')
-    fused = Flatten(name='bn_flat')(fused)
-    fused = Dense(bottleneck, activation='relu', name='bn_dense')(fused)
+    fused = bottleneck_block(fused.shape, latent_size, bottleneck, spatial_filters)(fused)
     
-    dec = Dense(latent_size * latent_size * spatial_filters[-1], activation='relu', name='dec_proj')(fused)
-    dec = Reshape((latent_size, latent_size, spatial_filters[-1]), name='dec_reshape')(dec)
-    
-    dec = spatial_decoder(dec, past_encs, fcast_encs)
-    
-    dec_tiled = Lambda(tile_for_decoder, output_shape=(in_t, in_w, in_h, spatial_filters[0]), name='dec_tile')(dec)
-    pos_enc = Lambda(create_pos_encoding, output_shape=(in_t, in_w, in_h, 1), name='pos_enc')(dec_tiled)
-    dec_input = concatenate([dec_tiled, pos_enc], axis=-1, name='dec_input')
-    
-    dec_lstm1 = ConvLSTM2D(temporal_filters[2], (3,3), padding='same', return_sequences=True, name='dec_lstm1')(
-        dec_input, initial_state=[h, c]
-    )
-    enc_tiled = Lambda(tile_for_decoder, output_shape=(in_t, in_w, in_h, spatial_filters[0]), name='enc_tile')(dec)
-    dec_lstm1 = Add()([dec_lstm1, enc_tiled])
-    
-    dec_lstm2 = ConvLSTM2D(temporal_filters[2], (3,3), padding='same', return_sequences=True, name='dec_lstm2')(dec_lstm1)
-    dec_lstm2 = Add()([dec_lstm2, dec_lstm1])
-    
-    output = Conv3D(1, (1,1,1), padding='same', activation='relu', name='output')(dec_lstm2)
+    dec = spatial_decoder(fused, past_encs, fcast_encs, spatial_filters, temporal_filters)
+    x = temporal_decoder([dec.shape, h.shape, c.shape], in_t, output_horizon, spatial_filters, temporal_filters)([dec, h, c])
+
+    output = Conv3D(1, (1,1,1), padding='same', activation='relu', name='output')(x)
     
     model = Model(inputs, output)
     print(f"Architecture: {latent_size}x{latent_size} | Parameters: {model.count_params():,}")
