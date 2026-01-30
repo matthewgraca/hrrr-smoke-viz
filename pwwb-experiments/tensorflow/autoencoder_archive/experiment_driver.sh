50e38719ac3011aea3bdd16b5348cbfbe419a033
Matthew Graca <matthew.graca@gmail.com>
2026-01-29 21:42:27 -0800

    added viz-only, removed viz when performing build run

--- pwwb-experiments/tensorflow/autoencoder_archive/experiment_driver.sh
+++ pwwb-experiments/tensorflow/autoencoder_archive/experiment_driver.sh
@@ -6,24 +6,33 @@
 # experiment name = string to be append to experiment description
 # data location = folder containing the training data (in the current directory)
 # is test = is/is not a test. turn false if you want a real run.
+# viz only = run only the visualization script, no training.
 
 MODEL_NAME='dual_autoencoder'
-LOSS_NAME='grid_mse'
-EXPERIMENT_NAME='0'
-DATA_LOC='preprocessed_cache'
-IS_TEST=false
+LOSS_NAME='grid_mae'
+EXPERIMENT_NAME='84x84'
+DATA_LOC='/mnt/wildfire/training-data/2026-01-27'
+IS_TEST=true
+VIZ_ONLY=false
+
+if [ "$VIZ_ONLY" = true ]; then
+    echo -e "\e[1mConducting visualizations only.\e[0m"
+    python vis.py \
+        "${MODEL_NAME}_${LOSS_NAME}_loss_${EXPERIMENT_NAME}" \
+        "$DATA_LOC"
+    exit 0
+fi
 
 if [ "$IS_TEST" = true ]; then
+    echo -e "\e[1mConducting test experiment only.\e[0m"
     python training.py \
         "$MODEL_NAME" \
         "$LOSS_NAME" \
         "$DATA_LOC" \
         -r "$EXPERIMENT_NAME" \
         -t
-    python vis.py \
-        "test_experiment" \
-        "$DATA_LOC"
 else
+    echo -e "\e[1mRunning full training sequence.\e[0m"
     python training.py \
         "$MODEL_NAME" \
         "$LOSS_NAME" \
